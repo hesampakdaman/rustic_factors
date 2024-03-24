436e1337cfca7ebc9692aedb36566237dcd04447
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-03-24 11:38:56 +0100

    Display now make use of superscript

--- src/factors.rs
+++ src/factors.rs
@@ -1,4 +1,9 @@
+use std::collections::BTreeMap;
+use std::fmt;
+
 use crate::Factorize;
+
+static SUPERSCRIPTS: [&str; 10] = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"];
 
 pub struct Factorization {
     number: u128,
@@ -14,12 +19,57 @@ impl Factorization {
     }
 
     pub fn display(&self) -> String {
-        let factors = self
-            .factors
+        let display = self
+            .frequencies()
             .iter()
-            .map(|p| p.to_string())
+            .map(|(&base, &exp)| format_factor(base, exp))
             .collect::<Vec<_>>()
             .join(" x ");
-        format!("{} = {}", self.number, factors)
+        format!("{} = {}", self.number, display)
+    }
+
+    fn frequencies(&self) -> BTreeMap<u128, usize> {
+        self.factors.iter().fold(BTreeMap::new(), |mut bmap, &n| {
+            *bmap.entry(n).or_insert(0) += 1;
+            bmap
+        })
+    }
+}
+
+impl fmt::Display for Factorization {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.display())
+    }
+}
+
+fn format_factor(base: u128, exp: usize) -> String {
+    fn format_exp(exp: usize) -> String {
+        if exp < 2 || exp > 9 {
+            format!("^{}", exp)
+        } else {
+            SUPERSCRIPTS[exp].to_string()
+        }
+    }
+    format!("{}{}", base, format_exp(exp))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn superscript() {
+        let number = 36;
+        let factors = vec![2, 2, 3, 3];
+        let actual = Factorization { number, factors }.display();
+        assert_eq!(actual, "36 = 2² x 3²");
+    }
+
+    #[test]
+    fn fallback() {
+        let number = 1024;
+        let factors = vec![2; 10];
+        let actual = Factorization { number, factors }.display();
+        assert_eq!(actual, "1024 = 2^10");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,16 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     if args.len() < 3 {
         eprintln!("Usage: {} <algorithm> <number>", args[0]);
         std::process::exit(1);
     }
-
     let method = &args[1];
     let n: u128 = args[2]
         .parse()
         .expect("Please provide a valid positive integer");
-
     match method.as_str() {
-        "trial_division" => println!("{}", Factorization::new(n, algorithms::TrialDivision).display()),
+        "trial_division" => println!("{}", Factorization::new(n, algorithms::TrialDivision)),
         _ => eprintln!("Unknown algorithm. Available options: trial_division"),
     }
 }
