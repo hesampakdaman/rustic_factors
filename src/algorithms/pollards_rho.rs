01076a2ef4e0ae9d2d96ea5c774edec4c7d9c268
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-04-01 15:14:29 +0200

    Factor out 2; generate psudorandom fn outside of pollards rho

--- src/algorithms/pollards_rho.rs
+++ src/algorithms/pollards_rho.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 mod utils;
 use std::collections::VecDeque;
+use std::ops::Range;
 
 use crate::primality_test::MillerRabin;
 use crate::traits::PrimalityTest;
@@ -9,18 +10,29 @@ use crate::Factorize;
 pub struct PollardsRho;
 
 impl Factorize for PollardsRho {
-    fn factorize(&self, n: u128) -> Vec<u128> {
+    fn factorize(&self, mut n: u128) -> Vec<u128> {
         if n <= 3 || MillerRabin.is_prime(n) {
             return vec![n];
         }
         let mut factors = vec![];
+        while n % 2 == 0 {
+            factors.push(2);
+            n /= 2;
+        }
+        if n <= 1 {
+            return factors;
+        }
         let mut queue = VecDeque::from([n]);
+        let mut f = generate_psudeorandom_fn(n);
         while let Some(m) = queue.pop_front() {
+            if m <= 1 {
+                continue;
+            }
             if MillerRabin.is_prime(m) {
                 factors.push(m);
                 continue;
             }
-            match pollards_rho(m) {
+            match pollards_rho(m, &f) {
                 FoundFactor::Prime(x) => {
                     factors.push(x);
                     queue.push_front(m / x)
@@ -29,8 +41,9 @@ impl Factorize for PollardsRho {
                     queue.push_front(x);
                     queue.push_front(m / x)
                 }
-                FoundFactor::Trivial(_) => {
-                    continue;
+                FoundFactor::Trivial(x) => {
+                    queue.push_front(x);
+                    f = generate_psudeorandom_fn(n);
                 }
             }
         }
@@ -39,12 +52,19 @@ impl Factorize for PollardsRho {
     }
 }
 
-fn pollards_rho(n: u128) -> FoundFactor {
+fn generate_psudeorandom_fn(n: u128) -> impl Fn(u128) -> u128 {
+    let c = random_integer(1..n);
+    move |x| (x * x + c) % n
+}
+
+fn random_integer(r: Range<u128>) -> u128 {
+    rand::thread_rng().gen_range(r)
+}
+
+fn pollards_rho(n: u128, psudorandom_fn: impl Fn(u128) -> u128) -> FoundFactor {
     let init = 2;
-    let c = rand::thread_rng().gen_range(1..n);
     let finished = |x: u128, y: u128| utils::gcd(x.abs_diff(y), n) != 1;
-    let psuedorandom = |x: u128| (x * x + c) % n;
-    let (tortoise, hare) = utils::floyds_cycle_detection(init, &psuedorandom, &finished);
+    let (tortoise, hare) = utils::floyds_cycle_detection(init, &psudorandom_fn, &finished);
     let divisor = utils::gcd(tortoise.abs_diff(hare), n);
     if divisor == 1 || divisor == n {
         FoundFactor::Trivial(divisor)
@@ -71,5 +91,7 @@ mod tests {
     #[test]
     fn simple() {
         assert_eq!(PollardsRho.factorize(8051), vec![83, 97]);
+        assert_eq!(PollardsRho.factorize(2u128.pow(10)), vec![2; 10]);
+        assert_eq!(PollardsRho.factorize(15), vec![3, 5]);
     }
 }
