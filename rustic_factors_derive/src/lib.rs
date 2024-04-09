62dae713920348591ba904086e68549cf66e60bd
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-04-09 23:40:33 +0200

    Remove use syn;

--- rustic_factors_derive/src/lib.rs
+++ rustic_factors_derive/src/lib.rs
@@ -1,6 +1,5 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
 
 #[proc_macro_derive(RecursivePrimeFactorization)]
 pub fn recursive_prime_factorization_derive(input: TokenStream) -> TokenStream {
