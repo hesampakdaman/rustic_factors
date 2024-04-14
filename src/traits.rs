521f705f68f1df9953a35364de02c976a6ee98cb
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-04-14 22:27:21 +0200

    Sort traits in traits.rs

--- src/traits.rs
+++ src/traits.rs
@@ -1,11 +1,11 @@
 use bnum::types::U512;
 
-pub trait PrimeFactorization {
-    fn prime_factorization(n: &U512) -> Vec<U512>;
-}
-
 pub trait Factorize {
     fn factorize(n: &U512) -> U512;
+}
+
+pub trait PrimeFactorization {
+    fn prime_factorization(n: &U512) -> Vec<U512>;
 }
 
 pub trait PrimalityTest {
